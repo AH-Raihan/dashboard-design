57d84af191966cea967c111aa3bb1f98f33c27ac
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2023-09-10 06:59:09 +0600

    Update main content sidebarscript.js

--- js/sidebarscript.js
+++ js/sidebarscript.js
@@ -29,7 +29,7 @@ darkLight.addEventListener("click", () => {
   body.classList.toggle("dark");
   if (body.classList.contains("dark")) {
     document.setI
-    darkLight.classList.replace("bx-sun", "bx-moon
+    darkLight.classList.replace("bx-sun", "bx-moon");
   } else {
     darkLight.classList.replace("bx-moon", "bx-sun");
   }
